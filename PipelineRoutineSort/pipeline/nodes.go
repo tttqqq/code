c9912cf4acb4b982929dd3b3d681ebc7041a66fa
tttqqq <tttqqq@foxmail.com>
2018-05-06 18:05:49 +0800

    实现简单的排序

--- PipelineRoutineSort/pipeline/nodes.go
+++ PipelineRoutineSort/pipeline/nodes.go
@@ -1,9 +1,32 @@
 package pipeline
 
-func ArraySource(a ...int) chan int {
+import (
+	"sort"
+)
+
+func ArraySource(a ...int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
+
+func InMemSort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		// Read into memory
+		a := []int{}
+		for v := range in {
+			a = append(a,v)
+		}
+		// Sort
+		sort.Ints(a)
+		//Output
+		for _,v := range a {
 			out <- v
 		}
 		close(out)
--- PipelineRoutineSort/pipelinedemo/pipelinedemo.go
+++ PipelineRoutineSort/pipelinedemo/pipelinedemo.go
@@ -7,11 +7,19 @@ import (
 
 func main() {
 	p := pipeline.ArraySource(3,2,6,7,4)
+	for v := range p {
+		fmt.Println(v)
+	}
+	
+	fmt.Println("---------华丽的分割线-------------")
+	p1 := pipeline.InMemSort(pipeline.ArraySource(3,2,6,7,4))
 	for {
-		if num,ok := <- p; ok {
+		if num,ok := <-p1; ok {
 			fmt.Println(num)
 		} else {
 			break
 		}
 	}
+
+
 }
