19bfe06ed58e7e583092a0296e357daf82342f4e
tttqqq <tttqqq@foxmail.com>
2018-05-16 21:58:17 +0800

    c++ hashmap

---
+++ hashmap/hashmap.cpp
@@ -0,0 +1,15 @@
+#include<hash_map>
+#include <string>
+
+using namespace std;
+
+int main(){
+    hash_map<int,sting> mymap;
+    mymap[9527] = "唐伯虎点秋香";
+    mymap[100000000]=="百万富翁的生活";
+    mymap[20000]="白领的工资底线";
+
+    if(mymap.find(20000) != mymap.end()){
+        cout<<"白领的工资底线"<<end;
+    }
+}
